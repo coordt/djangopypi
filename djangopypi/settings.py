e126deeb4cd37da88d6a57510050ceb0e14636b8
Corey Oordt <coreyoordt@gmail.com>
2012-04-05 05:09:31 -0400

    Convert existing settings into a dict and raise deprecation warnings about existing settings.

--- djangopypi/settings.py
+++ djangopypi/settings.py
@@ -1,17 +1,14 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.importlib import import_module
 
-# This is disabled on pypi.python.org, can be useful if you make mistakes
-if not hasattr(settings,'DJANGOPYPI_ALLOW_VERSION_OVERWRITE'):
-    settings.DJANGOPYPI_ALLOW_VERSION_OVERWRITE = False
+import warnings
 
-""" The upload_to argument for the file field in releases. This can either be 
-a string for a path relative to your media folder or a callable. For more 
-information, see http://docs.djangoproject.com/ """
-if not hasattr(settings,'DJANGOPYPI_RELEASE_UPLOAD_TO'):
-    settings.DJANGOPYPI_RELEASE_UPLOAD_TO = 'dists'
-
-if not hasattr(settings,'DJANGOPYPI_OS_NAMES'):
-    settings.DJANGOPYPI_OS_NAMES = (
+DEFAULT_SETTINGS = {
+    'ALLOW_VERSION_OVERWRITE': False, # This is disabled on pypi.python.org, can be useful if you make mistakes
+    'RELEASE_UPLOAD_TO': 'dists', # The upload_to argument for the file field in releases. This can either be a string for a path relative to your media folder or a callable.
+    'RELEASE_FILE_STORAGE': settings.DEFAULT_FILE_STORAGE,
+    'OS_NAMES': (
         ("aix", "AIX"),
         ("beos", "BeOS"),
         ("debian", "Debian Linux"),
@@ -28,30 +25,24 @@ if not hasattr(settings,'DJANGOPYPI_OS_NAMES'):
         ("solaris", "SUN Solaris"),
         ("suse", "SuSE Linux"),
         ("yellowdog", "Yellow Dog Linux"),
-    )
-
-if not hasattr(settings,'DJANGOPYPI_ARCHITECTURES'):
-    settings.DJANGOPYPI_ARCHITECTURES = (
+    ),
+    'ARCHITECTURES': (
         ("alpha", "Alpha"),
         ("hppa", "HPPA"),
         ("ix86", "Intel"),
         ("powerpc", "PowerPC"),
         ("sparc", "Sparc"),
         ("ultrasparc", "UltraSparc"),
-    )
-
-if not hasattr(settings,'DJANGOPYPI_DIST_FILE_TYPES'):
-    settings.DJANGOPYPI_DIST_FILE_TYPES = (
+    ),
+    'DIST_FILE_TYPES': (
         ('sdist','Source'),
         ('bdist_dumb','"dumb" binary'),
         ('bdist_rpm','RPM'),
         ('bdist_wininst','MS Windows installer'),
         ('bdist_egg','Python Egg'),
         ('bdist_dmg','OS X Disk Image'),
-    )
-
-if not hasattr(settings,'DJANGOPYPI_PYTHON_VERSIONS'):
-    settings.DJANGOPYPI_PYTHON_VERSIONS = (
+    ),
+    'PYTHON_VERSIONS': (
         ('any','Any i.e. pure python'),
         ('2.1','2.1'),
         ('2.2','2.2'),
@@ -63,10 +54,8 @@ if not hasattr(settings,'DJANGOPYPI_PYTHON_VERSIONS'):
         ('3.0','3.0'),
         ('3.1','3.1'),
         ('3.2','3.2'),
-    )
-
-if not hasattr(settings, 'DJANGOPYPI_METADATA_FIELDS'):
-    settings.DJANGOPYPI_METADATA_FIELDS = {
+    ),
+    'METADATA_FIELDS': {
         '1.0': ('platform','summary','description','keywords','home_page',
                 'author','author_email', 'license', ),
         '1.1': ('platform','supported_platform','summary','description',
@@ -76,47 +65,60 @@ if not hasattr(settings, 'DJANGOPYPI_METADATA_FIELDS'):
                 'keywords','home_page','download_url','author','author_email',
                 'maintainer','maintainer_email','license','classifier',
                 'requires_dist','provides_dist','obsoletes_dist',
-                'requires_python','requires_external','project_url')}
-
-if not hasattr(settings, 'DJANGOPYPI_METADATA_FORMS'):
-    from djangopypi.forms import Metadata10Form, Metadata11Form, Metadata12Form
-    settings.DJANGOPYPI_METADATA_FORMS = {
-        '1.0': Metadata10Form,
-        '1.1': Metadata11Form,
-        '1.2': Metadata12Form}
-
-if not hasattr(settings, 'DJANGOPYPI_FALLBACK_VIEW'):
-    from djangopypi.views import releases 
-    settings.DJANGOPYPI_FALLBACK_VIEW = releases.index
-
-if not hasattr(settings,'DJANGOPYPI_ACTION_VIEWS'):
-    from djangopypi.views import distutils
-    
-    settings.DJANGOPYPI_ACTION_VIEWS = {
-        "file_upload": distutils.register_or_upload, #``sdist`` command
-        "submit": distutils.register_or_upload, #``register`` command
-        "list_classifiers": distutils.list_classifiers, #``list_classifiers`` command
-    }
-
-if not hasattr(settings,'DJANGOPYPI_XMLRPC_COMMANDS'):
-    from djangopypi.views import xmlrpc
-    
-    settings.DJANGOPYPI_XMLRPC_COMMANDS = {
-        'list_packages': xmlrpc.list_packages,
-        'package_releases': xmlrpc.package_releases,
-        'release_urls': xmlrpc.release_urls,
-        'release_data': xmlrpc.release_data,
+                'requires_python','requires_external','project_url')
+    },
+    'METADATA_FORMS': {
+        '1.0': 'djangopypi.forms.Metadata10Form',
+        '1.1': 'djangopypi.forms.Metadata11Form',
+        '1.2': 'djangopypi.forms.Metadata12Form'
+    },
+    'FALLBACK_VIEW': 'djangopypi.views.releases.index',
+    'ACTION_VIEWS': {
+        "file_upload": 'djangopypi.views.distutils.register_or_upload', #``sdist`` command
+        "submit": 'djangopypi.views.distutils.register_or_upload', #``register`` command
+        "list_classifiers": 'djangopypi.views.distutils.list_classifiers', #``list_classifiers`` command
+    },
+    'XMLRPC_COMMANDS': {
+        'list_packages': 'djangopypi.views.xmlrpc.list_packages',
+        'package_releases': 'djangopypi.views.xmlrpc.package_releases',
+        'release_urls': 'djangopypi.views.xmlrpc.release_urls',
+        'release_data': 'djangopypi.views.xmlrpc.release_data',
         #'search': xmlrpc.search, Not done yet
         #'changelog': xmlrpc.changelog, Not done yet
         #'ratings': xmlrpc.ratings, Not done yet
-    }
+    },
+    'PROXY_BASE_URL': 'http://pypi.python.org/simple',
+    'PROXY_MISSING': False,
+    'MIRRORING': False,
+}
 
-""" These settings enable proxying of packages that are not in the local index 
-to another index, http://pypi.python.org/ by default. This feature is disabled 
-by default and can be enabled by setting DJANGOPYPI_PROXY_MISSING to True in 
-your settings file. """
-if not hasattr(settings, 'DJANGOPYPI_PROXY_BASE_URL'):
-    settings.DJANGOPYPI_PROXY_BASE_URL = 'http://pypi.python.org/simple'
+USER_SETTINGS = DEFAULT_SETTINGS.copy()
+USER_SETTINGS.update(getattr(settings, 'DJANGOPYPI_SETTINGS', {}))
 
-if not hasattr(settings, 'DJANGOPYPI_PROXY_MISSING'):
-    settings.DJANGOPYPI_PROXY_MISSING = False
+ORIGINAL_SETTINGS = (
+    'DJANGOPYPI_ALLOW_VERSION_OVERWRITE',
+    'DJANGOPYPI_RELEASE_UPLOAD_TO',
+    'DJANGOPYPI_OS_NAMES',
+    'DJANGOPYPI_ARCHITECTURES',
+    'DJANGOPYPI_DIST_FILE_TYPES',
+    'DJANGOPYPI_PYTHON_VERSIONS',
+    'DJANGOPYPI_METADATA_FIELDS',
+    'DJANGOPYPI_METADATA_FORMS',
+    'DJANGOPYPI_FALLBACK_VIEW',
+    'DJANGOPYPI_ACTION_VIEWS',
+    'DJANGOPYPI_XMLRPC_COMMANDS',
+    'DJANGOPYPI_PROXY_BASE_URL',
+    'DJANGOPYPI_PROXY_MISSING',
+    'DJANGOPYPI_MIRRORING',
+)
+
+for setting in ORIGINAL_SETTINGS:
+    value = getattr(settings, setting, False)
+    if value:
+        message = "%s is deprecated. Please use DJANGOPYPI_SETTINGS[%s] instead."
+        new_setting = setting.replace('DJANGOPYPI_', '')
+        warnings.warn(message % (setting, new_setting), DeprecationWarning)
+        USER_SETTINGS[new_setting] = value
+        globals()[setting] = value
+
+globals().update(USER_SETTINGS)
