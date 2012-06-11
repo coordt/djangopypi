c4345722b811158130b749ceb5511c9bd8e64ecf
Corey Oordt <coreyoordt@gmail.com>
2012-06-11 08:07:51 -0400

    Added the owner into the package admin

--- userpypi/admin.py
+++ userpypi/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from userpypi.models import *
 from userpypi.settings import MIRRORING
 
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner',)
+    search_fields = ('name',)
 
-
-admin.site.register(Package)
+admin.site.register(Package, PackageAdmin)
 admin.site.register(Release)
 admin.site.register(Classifier)
 admin.site.register(Distribution)
